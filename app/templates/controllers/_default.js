7e742285a53d9e4c1268ecb63ae8eced3a0aa1c2
Justin Gardner <justin@justingardner.com>
2015-05-14 23:48:32 -0400

    added enable database option

--- app/templates/controllers/_default.js
+++ app/templates/controllers/_default.js
@@ -1,25 +1,51 @@
+'use strict';
+
 var _ = require('lodash');
-var moment = require('moment');
 var couchdb = require('../services/couchdb');
+var config = require('../config/<%= sluggedAppname %>');
 
 var db = couchdb.database('<%= sluggedAppname %>');
 
 var controller = {};
 
-controller.all = function (req, res, next) {
-    db.view('<%= sluggedAppname %>/all', { descending: true }, function (err, doc) {
-        if (err) return next(err);
-       
-        var allTheThings = [];
+controller.getAll = function (req, res, next) {
+	if (config.get('<%= camelCasedAppname %>Config:enable_database')) {
+        db.view('<%= sluggedAppname %>/all', { descending: true }, function (err, doc) {
+            if (err) return next(err);
+           
+            var allTheThings = [];
 
-        _.forEach(doc, function(thing) { 
-            allTheThings.push(thing.value); 
+            _.forEach(doc, function(thing) { 
+                allTheThings.push(thing.value); 
+            });
+
+            req.all = allTheThings;
+            
+            next();
         });
+    }  else {
+    	req.all = [{ 'id': 1, 'title': 'Shut the front door' }];
+    	next();
+    }
+};
 
-        req.all = allTheThings;
-        
-        next();
-    });
-}
+controller.getOne = function (req, res, next) {
+	if (config.get('<%= camelCasedAppname %>Config:enable_database')) {
+        var id = req.params.id;
+        db.get(id, function (err, doc) {
+            if (err) return next(err);
+
+            req.one = _({})
+                .assign(doc)
+                .omit(['_rev', '_deleted_conflicts'])
+                .value();
+
+            next();
+        });
+    } else {
+    	req.one = { 'id': 1, 'title': 'Shut the front door' };
+    	next();
+    }
+};
 
 exports = module.exports = controller;
